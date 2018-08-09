f9858d9c9ab52513ed86180297efcc969c326c63
Mohit Delhiwala <mohithem@buffalo.edu>
2018-08-09 12:52:24 -0400

    Setup project scaffold. Basic HTML rendering works

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+let config = {
+  entry: ["./app.tsx"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: "ts-loader",
+      exclude: /node_modules/
+    }]
+  }
+};
+
+module.exports = config;
